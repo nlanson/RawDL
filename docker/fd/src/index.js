d4393a36fc8f4d75567b5330b34ff0fc9559e5e4
Noah Lanson <lanson.noah@gmail.com>
2021-02-06 16:59:19 +1100

    Docker Images for Each MOde

---
+++ docker/fd/src/index.js
@@ -0,0 +1,15 @@
+const { rawdl } = require('rawdl');
+
+async function main() {
+    const api_keys = {
+        username: process.env.API_USERNAME,
+        password: process.env.API_PASSWORD,
+        folder: process.env.API_FOLDER
+    };
+    let time = new Date();
+    console.log(`Starting Rawdl in Full Delete Mode [${time.getHours()}:${time.getMinutes()} ${time.getDate()}/${time.getMonth()}]`);
+    let ap = new rawdl.rawdl.AutoPilot('/data/shows.json', 'https://subsplease.org/rss/?t&r=1080', api_keys,'/data/downloads');
+    await ap.full_delete();
+}
+
+main();
